c48208089aab0030de122f9c17e45b3722b44449
Mike <mike.lu.92@gmail.com>
2014-04-29 11:40:54 -0700

    newest version of database made, please change you cse135 database to this one

---
+++ cse135.sql
@@ -0,0 +1,280 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: adminpack; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION adminpack IS 'administrative functions for PostgreSQL';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: categories; Type: TABLE; Schema: public; Owner: mikel; Tablespace: 
+--
+
+CREATE TABLE categories (
+    id integer NOT NULL,
+    name text,
+    description text
+);
+
+
+ALTER TABLE public.categories OWNER TO mikel;
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE; Schema: public; Owner: mikel
+--
+
+CREATE SEQUENCE categories_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.categories_id_seq OWNER TO mikel;
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: mikel
+--
+
+ALTER SEQUENCE categories_id_seq OWNED BY categories.id;
+
+
+--
+-- Name: products; Type: TABLE; Schema: public; Owner: mikel; Tablespace: 
+--
+
+CREATE TABLE products (
+    id integer NOT NULL,
+    name text NOT NULL,
+    sku integer NOT NULL,
+    price numeric(5,2) NOT NULL,
+    category integer,
+    owner integer NOT NULL
+);
+
+
+ALTER TABLE public.products OWNER TO mikel;
+
+--
+-- Name: products_id_seq; Type: SEQUENCE; Schema: public; Owner: mikel
+--
+
+CREATE SEQUENCE products_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.products_id_seq OWNER TO mikel;
+
+--
+-- Name: products_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: mikel
+--
+
+ALTER SEQUENCE products_id_seq OWNED BY products.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: mikel; Tablespace: 
+--
+
+CREATE TABLE users (
+    id integer NOT NULL,
+    name text NOT NULL,
+    age integer NOT NULL,
+    state character(2) NOT NULL,
+    owner boolean NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO mikel;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: mikel
+--
+
+CREATE SEQUENCE users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO mikel;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: mikel
+--
+
+ALTER SEQUENCE users_id_seq OWNED BY users.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: mikel
+--
+
+ALTER TABLE ONLY categories ALTER COLUMN id SET DEFAULT nextval('categories_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: mikel
+--
+
+ALTER TABLE ONLY products ALTER COLUMN id SET DEFAULT nextval('products_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: mikel
+--
+
+ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+
+--
+-- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: mikel
+--
+
+COPY categories (id, name, description) FROM stdin;
+1	test	description
+\.
+
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE SET; Schema: public; Owner: mikel
+--
+
+SELECT pg_catalog.setval('categories_id_seq', 1, true);
+
+
+--
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: mikel
+--
+
+COPY products (id, name, sku, price, category, owner) FROM stdin;
+\.
+
+
+--
+-- Name: products_id_seq; Type: SEQUENCE SET; Schema: public; Owner: mikel
+--
+
+SELECT pg_catalog.setval('products_id_seq', 1, false);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: mikel
+--
+
+COPY users (id, name, age, state, owner) FROM stdin;
+\.
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: mikel
+--
+
+SELECT pg_catalog.setval('users_id_seq', 1, false);
+
+
+--
+-- Name: categories_pkey; Type: CONSTRAINT; Schema: public; Owner: mikel; Tablespace: 
+--
+
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: products_pkey; Type: CONSTRAINT; Schema: public; Owner: mikel; Tablespace: 
+--
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT products_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: products_sku_key; Type: CONSTRAINT; Schema: public; Owner: mikel; Tablespace: 
+--
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT products_sku_key UNIQUE (sku);
+
+
+--
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: mikel; Tablespace: 
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: products_category_fkey; Type: FK CONSTRAINT; Schema: public; Owner: mikel
+--
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT products_category_fkey FOREIGN KEY (category) REFERENCES categories(id);
+
+
+--
+-- Name: products_owner_fkey; Type: FK CONSTRAINT; Schema: public; Owner: mikel
+--
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT products_owner_fkey FOREIGN KEY (owner) REFERENCES users(id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
