e0ea5e5f8078394299f6a911c54d7c68699ffa2c
mikel <mike.lu.92@gmail.com>
2014-06-05 13:06:52 -0700

    added sql files to make it work

--- sql.sql
+++ sql.sql
@@ -62,33 +62,3 @@ CREATE TABLE UserSales (
     sumamt	INTEGER
 );
 
-CREATE TABLE TwoDSales (
-    uid		INTEGER REFERENCES users (id) ON DELETE CASCADE,
-    state	TEXT NOT NULL,
-    pid		INTEGER REFERENCES products (id) ON DELETE CASCADE,
-    category	TEXT NOT NULL,
-    sumamt	INTEGER
-);
-
-INSERT INTO productsales (SELECT products.id, states.state, sum(quantity*sales.price) AS sumamt
-FROM products 
-LEFT JOIN sales ON products.id = pid
-JOIN users ON users.id = uid
-JOIN states ON states.state = users.state
-GROUP BY products.id, states.state);
-
-INSERT INTO UserSales (
-SELECT users.id, categories.name, SUM(quantity*sales.price) AS sumamt
-FROM users 
-LEFT JOIN sales ON users.id = uid
-JOIN products ON pid = products.id
-JOIN categories ON cid = categories.id
-GROUP BY users.id, categories.name
-);
-
-SELECT uid, users.state, pid, categories.name as cname, SUM(quantity*sales.price) AS sumamtFROM sales
-RIGHT JOIN users ON uid = users.id
-RIGHT JOIN products ON pid = products.id
-JOIN categories on products.cid = categories.id
-group by uid, users.state, pid, categories.name;
-
